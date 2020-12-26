ea56bf6b5ebf5dfdd7bc2b959415ca26b42f4fab
juhyunham <judev0227@gmail.com>
2020-12-26 23:42:17 +0900

    navbar getBoundingclinet Height

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 document.addEventListener(`DOMContentLoaded`, function() {
     //navbar sticky
+    const navbar = document.querySelector(`#navbar`);
+    const navbarHeight = navbar.getBoundingClientRect().height;
     window.addEventListener(`scroll`, function() {
-        if (window.scrollY > 0) {
+        if (window.scrollY > navbarHeight) {
             document.querySelector(`#navbar`).classList.add(`fixed`);
         } else {
             document.querySelector(`#navbar`).classList.remove(`fixed`);
