0349781c3618a376cd6732a12dcd7e0b4a864721
juhyunham <judev0227@gmail.com>
2021-01-11 23:30:44 +0900

    007 upgrade translate (paint, composite)

--- browser/chapter5/other/007.js
+++ browser/chapter5/other/007.js
@@ -2,19 +2,21 @@ const horizontal = document.querySelector(`.horizontal`);
 const vertical = document.querySelector(`.vertical`);
 const target = document.querySelector(`.target`);
 const coordinate = document.querySelector(`.coordinate`);
+const targetRect = target.getBoundingClientRect()
+const targetHalfWidth = targetRect.width / 2
+const targetHalfHeight = targetRect.height / 2
 
 document.addEventListener(`mousemove`, (event) => {
     const x = event.clientX;
     const y = event.clientY;
-    
-    vertical.style.left = `${x}px`
-    horizontal.style.top = `${y}px`
 
-    target.style.top = `${y}px`
-    target.style.left = `${x}px`
 
-    coordinate.style.top = `${y}px`
-    coordinate.style.left = `${x}px`
+    vertical.style.transform = `translateX(${x}px)`
+    horizontal.style.transform = `translateY(${y}px)`
+
+    target.style.transform = `translate(${x - (targetHalfWidth)}px, ${y - (targetHalfHeight)}px)`
 
     coordinate.innerText = `${x}px, ${y}px`
+
+    coordinate.style.transform = `translate(${x + 20}px, ${y + 20}px)`
 })
