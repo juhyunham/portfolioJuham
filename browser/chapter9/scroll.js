9aedb8dd73bc6688dbacd6a7d999c3592f1d559b
juhyunham <judev0227@gmail.com>
2021-01-15 15:35:17 +0900

    scroll header remove

--- browser/chapter9/scroll.js
+++ browser/chapter9/scroll.js
@@ -13,13 +13,14 @@ document.addEventListener(`DOMContentLoaded`, () => {
 
     const sections = document.querySelectorAll(`.section`)
     const menu = document.querySelectorAll(`.header li`);
-    let headerHeight = document.querySelector(`.header`).offsetHeight; 
+    const header = document.querySelector(`.header`)
+    let headerHeight = header.offsetHeight; 
     let sectionTop = [];
 
     for (let i = 0; i < sections.length; i++) {
         sectionTop.push(sections[i].getBoundingClientRect().top + window.scrollY)
     }
-
+    
     const sectionTopFun = () => {
         let temp = 0;
         for (let i = 0; i < sectionTop.length; i++) {
@@ -33,6 +34,15 @@ document.addEventListener(`DOMContentLoaded`, () => {
         }
         
         menu[temp].classList.add(`active`)
+
+        // 형변환 쓴거 이상하다고 느껴짐.
+        if (Number(menu[temp].dataset.menu) === menu.length - 1) {
+            if (sectionTop[temp] < window.scrollY) {
+                header.classList.add(`invisible`)
+            }
+        } else {
+            header.classList.remove(`invisible`)
+        }
     }
 
     window.addEventListener(`scroll`, () => {
@@ -42,6 +52,8 @@ document.addEventListener(`DOMContentLoaded`, () => {
     menu.forEach(item => {
         item.addEventListener(`click`, function() {
             const _this = this;
+
+            // 사실 dataset으로 쓴게 고민되긴함.
             const _thisTop =  sectionTop[_this.dataset.menu] - headerHeight;
 
             window.scrollTo({top:  _thisTop,  behavior: 'smooth'});
